f137d771368c826c176f896a4d6d6516a6ad3f55
Ramzy Ahamed (JIT) - External <ramzya@jkintranet.com>
2023-08-04 00:45:52 +0530

    footer section

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     const year = date.getFullYear()
     return (
         <footer className="bg-neutral-900 text-center text-white mt-4">
-            <div className="container px-6 pt-6">
+            <div className="px-6 pt-6 w-[100%]">
                 <div className="mb-6 flex justify-center">
                     {/* facebook */}
                     <a href="https://web.facebook.com/Ramzyrox" target="_blank" type="button" className="m-1 h-9 w-9 rounded-full border-2 border-white uppercase leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0" data-te-ripple-init data-te-ripple-color="light">
